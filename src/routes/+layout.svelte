d64ce115132d802304a69dae9ed3321b492af134
many bear <98581612+6666dustry@users.noreply.github.com>
2023-05-16 21:02:40 +0900

    version1.00

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,14 @@
 <slot />
 <footer>
 	<p><a target="_blank" href="https://otologic.jp/">効果音:OtoLogic</a></p>
+	<p id="version">version:1.00</p>
 </footer>
 
 <style>
 	h1 {
 		font-size: 100px;
+	}
+	#version {
+		font-size: 15px;
 	}
 </style>
