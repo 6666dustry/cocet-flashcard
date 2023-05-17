44f45fa7d1e81d114f6a04f0f67293e9626e6d65
many bear <98581612+6666dustry@users.noreply.github.com>
2023-05-17 20:49:07 +0900

    fix title.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
 	import '../common.css';
 	import Config from '../lib/Config.svelte';
-	let text = Math.random() < 0.2 ? 'cocet' : 'COCET';
+	let text = Math.random() < 0.2 ? 'cocet flash card' : 'COCET FLASH CARD';
 </script>
 
 <h1>{text}</h1>
 <slot />
 <footer>
 	<p><a target="_blank" href="https://otologic.jp/">効果音:OtoLogic</a></p>
-	<p id="version">version:1.0.0</p>
+	<p id="version">version:1.0.1</p>
 </footer>
 
 <style>
