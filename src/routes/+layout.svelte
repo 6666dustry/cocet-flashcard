fb70e9971e0cabd8e3d5a9ef2ffc7b4399b3f2d7
many bear <98581612+6666dustry@users.noreply.github.com>
2023-05-18 17:57:20 +0900

    1.0.2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,14 @@
 	import '../common.css';
 	import Config from '../lib/Config.svelte';
 	let text = Math.random() < 0.2 ? 'cocet flash card' : 'COCET FLASH CARD';
+	let version = '1.0.2';
 </script>
 
 <h1>{text}</h1>
 <slot />
 <footer>
 	<p><a target="_blank" href="https://otologic.jp/">効果音:OtoLogic</a></p>
-	<p id="version">version:1.0.1</p>
+	<p id="version">version:{version}</p>
 </footer>
 
 <style>
