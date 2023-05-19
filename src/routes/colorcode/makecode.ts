d0c4520bf9cbd06cdbfc39155e6b6ad15d74a5a9
many bear <98581612+6666dustry@users.noreply.github.com>
2023-05-19 22:31:03 +0900

    color code values.

---
+++ src/routes/colorcode/makecode.ts
@@ -0,0 +1,19 @@
+const codes = {
+  black: [0, 0, 1e0],
+  brown: [1, 1, 1e1, 1],
+  red: [2, 2, 1e2, 2],
+  orange: [3, 3, 1e3],
+  yellow: [4, 4, 1e4],
+  green: [5, 5, 1e5],
+  blue: [6, 6, 1e6],
+  purple: [7, 7, 1e7],
+  gray: [8, 8, 1e8],
+  white: [9, 9, 1e9],
+  silver: [null, null, 1e-1, 5],
+  gold: [null, null, 1e-2, 10],
+  none: [null, null, null, 20]
+};
+const hasFour = ["brown", "red", "silver", "gold", "none"];
+export function makeCode() {
+
+}
