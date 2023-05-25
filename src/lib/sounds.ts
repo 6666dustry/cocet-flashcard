3ce30953cc8b361a7888637d68905c525b6ded12
many bear <98581612+6666dustry@users.noreply.github.com>
2023-05-25 21:52:12 +0900

    optimizeeee!!!!

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -7,11 +7,6 @@ import w2 from "../assets/sounds/wrong/2.mp3";
 import w3 from "../assets/sounds/wrong/3.mp3";
 import w4 from "../assets/sounds/wrong/4.mp3";
 import w5 from "../assets/sounds/wrong/5.mp3";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-const audioContext = new AudioContext();
 const corrects = [c1, c2, c3, c4].map(x => new Audio(x));
 const wrongs = [w1, w2, w3, w4, w5].map(x => new Audio(x));
 
@@ -21,9 +16,6 @@ export function playSound(correct: boolean) {
     play = corrects[Math.floor(Math.random() * corrects.length)];
   } else {
     play = wrongs[Math.floor(Math.random() * wrongs.length)];
-  }
-  if (audioContext.state === 'suspended') {
-    audioContext.resume();
   }
   play.play();
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,10 @@
 <script lang="ts">
-	import { playSound } from '$lib/sounds.js';
 	import speak from '$lib/utils/speak.js';
-	import { fly, scale } from 'svelte/transition';
-	import { onMount } from 'svelte';
 	import Words from './Words.svelte';
-	import { getQuestion, words } from './words.js';
-	const delay = 500;
-	let currentWord = getQuestion()[0];
-	let currentIndex = 0;
-	let startTime = 0;
-	let missCount = 0;
-	let show = false;
-	let perKey = 0;
-
-	const startGame = () => {
-		currentIndex = 0;
-		startTime = Date.now();
-		currentWord = getQuestion()[0];
-		missCount = 0;
-	};
-	words.subscribe(() => {
-		startGame();
-	});
-
-	const endGame = () => {
-		const elapsedTime = Date.now() - startTime;
-		perKey = Math.ceil((currentWord.word.length / (elapsedTime / 1000)) * 100) / 100;
-		speak(currentWord.word);
-		playSound(true);
-	};
-
-	const handleInput = (key: string) => {
-		if (key === currentWord.word[currentIndex]) {
-			currentIndex = currentIndex + 1;
-			if (currentIndex + 1 > currentWord.word.length) {
-				endGame();
-				setTimeout(() => {
-					startGame();
-				}, delay);
-			}
-		} else {
-			missCount = missCount + 1;
-			show = true;
-			setTimeout(() => {
-				show = false;
-			}, delay);
-		}
-	};
-	const call = (event: any) => {
-		const key = event.key.toLowerCase();
-		handleInput(key);
-	};
-	onMount(() => {
-		currentWord = getQuestion()[0];
-	});
+	import { words } from './words.js';
+	import Short from './Short.svelte';
 </script>
 
-<svelte:window on:keydown={call} />
 <svelte:head>
 	<style>
 		html {
@@ -74,6 +22,9 @@
 	on:click={() => {
 		let index = 0;
 		let word = true;
+		/**
+		 * Infinity loop of speaking.
+		 */
 		function loop() {
 			const uttr = speak($words[index][word ? 'word' : 'translation'], {
 				lang: word ? 'en-US' : 'ja-JP',
@@ -93,23 +44,7 @@
 		loop();
 	}}>ラジオ</button
 >
-<div>
-	<p id="time">
-		{perKey}{' '}key/s {missCount}
-		{missCount <= 1 ? 'miss' : 'misses'}
-	</p>
-	{#key currentWord}
-		<p in:fly={{ delay: delay + 1, x: 200 }} out:fly={{ x: -200 }} id="word">
-			{currentWord.translation}
-		</p>
-		<p id="input" out:scale={{}}>
-			{currentWord.word.slice(0, currentIndex)}
-			{#if show}
-				<span class="highlight">{currentWord.word.slice(currentIndex)}</span>
-			{/if}
-		</p>
-	{/key}
-</div>
+<Short />
 <Words />
 <div id="description">
 	<h2>使い方</h2>
@@ -122,17 +57,6 @@
 <style>
 	p {
 		font-size: 40px;
-	}
-
-	span {
-		font-size: 40px;
-	}
-	#word {
-		font-family: 'Dela Gothic One';
-		transition: all 500ms;
-	}
-	#input {
-		min-height: 1em;
 	}
 	#description * {
 		font-size: 20px;
